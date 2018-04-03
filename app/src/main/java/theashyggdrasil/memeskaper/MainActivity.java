7baee038d35781e112aee59880f778bdb87563b0
Tucker Langseth <D6XB@area1.eurofins.local>
2018-04-03 14:49:48 -0400

    update

--- app/src/main/java/theashyggdrasil/memeskaper/MainActivity.java
+++ app/src/main/java/theashyggdrasil/memeskaper/MainActivity.java
@@ -1,7 +1,10 @@
 package theashyggdrasil.memeskaper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnLoad = findViewById(R.id.btnLoadImage);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), UploadImage.class);
+                startActivity(intent);
+            }
+        });
+
+        final Button btnWebImage = findViewById(R.id.btnUrlImage);
+        btnWebImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), WebImage.class);
+                startActivity(intent);
+            }
+        });
+
+        final Button btnFakeText = findViewById(R.id.btnFakeText);
+        btnFakeText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(getApplicationContext(), FakeText.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/theashyggdrasil/memeskaper/UploadImage.java
@@ -0,0 +1,36 @@
+package theashyggdrasil.memeskaper;
+
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class UploadImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_image);
+    }
+
+
+
+
+
+
+
+
+}
